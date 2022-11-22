618594967ecb0d36cafe09674585f7d9a5ab5ddb
vinay-kuma <Vinayvins9948@gmail.com>
2022-11-22 12:30:52 +0530

    syam

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -5,51 +5,50 @@ import validateForm from 'src/app/helpers/Validateform';
 import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  type: string = "Password";
-  isText: boolean =false;
-  eyeIcon: string = "fa-eye-slash"
-  signUpForm!: FormGroup; 
-  constructor(private fb : FormBuilder , private auth: AuthService, private router : Router ) { }
+  type: string = "Password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
+  signUpForm!: FormGroup; 
+  constructor(private fb : FormBuilder , private auth: AuthService, private router : Router ) { }
 
-  ngOnInit(): void {
-    this.signUpForm = this.fb.group({
-      firstName: ['',Validators.required],
-      lastName: ['',Validators.required],
-      userName: ['',Validators.required],
-      email: ['',Validators.required],
-      Password: ['',Validators.required],
-    })
-  }
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['',Validators.required],
+      lastName: ['',Validators.required],
+      username: ['',Validators.required],
+      email: ['',Validators.required],
+      password: ['',Validators.required],
+    })
+  }
 
-  hideShowPass(){
-    this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
-    this.isText ? this.type = " text": this.type = "Password";
-  }
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = " text": this.type = "Password";
+  }
 
-  onSignup(){
-  if(this.signUpForm.valid){
-    this.auth.signUp(this.signUpForm.value)
-    .subscribe({
-      next:(res=>{
-        alert(res.message);
-         this.signUpForm.reset();   
-         this.router.navigate(['login']);
-      }),
-      error:(err=>{
-        alert(err?.error.message)
-      })
-    })
-    console.log(this.signUpForm.value)
-  }
-  else{
-    validateForm.validateAllFormFields(this.signUpForm)
-  }
+  onSignup(){
+  if(this.signUpForm.valid){
+    this.auth.signUp(this.signUpForm.value)
+    .subscribe({
+      next:(res=>{
+        alert(res.message)
+      })
+      ,error:(err=>{
+        alert(err?.Message)
+// alert(err.message)
+      })
+    })
+    console.log(this.signUpForm.value)
+  }
+  else{
+    validateForm.validateAllFormFields(this.signUpForm)
+  }
 }
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private baseUrl:string = "https://localhost:7048/api/User/"
+   baseUrl:string = 'https://localhost:7048/';
   constructor(private http : HttpClient) { }
 
-  signUp(userObj:any){
-    return this.http.post<any>('${this.baseUrl}register', userObj);
+  // signUp(userObj:any){
+  //   return this.http.post<any>(this.baseUrl + 'api/register', userObj);
+  // }
+
+  // login(loginObj:any){
+  //   return this.http.post<any>('${this.baseUrl}authenticate', loginObj);
+  // }
+
+
+  signUp(userObj:any) : Observable<any>{
+    return this.http.post<any>(this.baseUrl + 'api/register', userObj);
   }
 
   login(loginObj:any){
-    return this.http.post<any>('${this.baseUrl}authenticate', loginObj);
+    return this.http.post<any>(this.baseUrl + 'api/authenticate', loginObj);
   }
 }
+// '${this.baseUrl}register'
